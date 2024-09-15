d3e0e2d1ff631bcc45bd25cd1508cab389d0c31f
tuannm99 <tuannm@foobla.com>
2024-09-15 07:09:45 +0000

    move repo

--- nodejs-in-depth/src/eventloop/index.js
+++ nodejs-in-depth/src/eventloop/index.js
@@ -21,49 +21,57 @@ console.log('clg 1')
 
 setTimeout(() => console.log('setimout1'), 0)
 setTimeout(() => {
-    console.log('setimout2')
-    process.nextTick(() => console.log('nextick inside timeout'))
+  console.log('setimout2')
+  process.nextTick(() => console.log('nextick inside timeout'))
 }, 0)
 setTimeout(() => console.log('setimout3'), 0)
 
 process.nextTick(() => {
-    console.log('❤❤❤ nextTick 1')
+  console.log('❤❤❤ nextTick 1')
 })
 process.nextTick(() => {
-    console.log('❤❤❤ nextTick 2')
-    process.nextTick(() => {
-        console.log('❤❤❤ nextTick nt3')
-    })
+  console.log('❤❤❤ nextTick 2')
+  process.nextTick(() => {
+    console.log('❤❤❤ nextTick nt3')
+  })
 })
 process.nextTick(() => {
-    console.log('❤❤❤ nextTick 3')
+  console.log('❤❤❤ nextTick 3')
 })
 
 Promise.resolve().then(() => {
-    console.log('❤❤❤ promise 1')
+  console.log('❤❤❤ promise 1')
 })
 Promise.resolve().then(() => {
-    console.log('❤❤❤ promise 2')
-    process.nextTick(() => {
-        console.log('❤❤❤ nextTick p2')
-    })
+  console.log('❤❤❤ promise 2')
+  process.nextTick(() => {
+    console.log('❤❤❤ nextTick p2')
+  })
 })
+
 Promise.resolve().then(() => {
-    console.log('❤❤❤ promise 3')
+  console.log('❤❤❤ promise 3')
 })
+
+new Promise((resolve) => {
+  console.log(1)
+  resolve()
+  console.log(2)
+}).then(() => console.log('thenn1'))
 
 console.log('clg 2')
 
 const fs = require('fs')
+const {resolve} = require('path')
 
 fs.readFile(__filename, () => {
-    console.log('done readfile')
-    setImmediate(() => console.log('setImmediate1 after readfile'))
+  console.log('done readfile')
+  setImmediate(() => console.log('setImmediate1 after readfile'))
 })
 
 process.nextTick(() => console.log('nextick after readfile'))
 Promise.resolve().then(() => {
-    console.log('❤❤❤ promise after readfile')
+  console.log('❤❤❤ promise after readfile')
 })
 setTimeout(() => console.log('setimout1 after readfile'), 0)
 
